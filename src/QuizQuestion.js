01a24df54b0a4e3e7087ccfdb5cda8d7490e0f42
Carlos Bourque <carlos@siros.global>
2020-07-04 17:32:03 +0200

    module 6

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -10,12 +10,24 @@ class QuizQuestion extends Component {
                 </section>
                 <section className="buttons">
                     <ul>
-                        <QuizQuestionButton button_text={this.props.quiz_question.answer_options[0]} />
+                        {this.props.quiz_question.answer_options.map((currentValue, index) => {
+                            return <QuizQuestionButton
+                                clickHandler={this.handleClick.bind(this)}
+                                button_text={currentValue}
+                                key={index} />
+                            })
+                        }
                     </ul>
                 </section>
             </main>
         );
     };
-}
+    handleClick(button_text)
+    {
+        if (button_text === this.props.quiz_question.answer) {
+            this.props.showNextQuestionHandler();
+        }
+    };
+};
 
 export default QuizQuestion;
