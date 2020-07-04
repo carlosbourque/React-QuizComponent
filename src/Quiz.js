e39cd11f987680f881de4530845ff200058b1afb
Carlos Bourque <carlosbourque@gmail.com>
2020-07-04 16:00:50 +0200

    module 5

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,18 +1,23 @@
 import React, { Component } from 'react';
 import QuizQuestion from './QuizQuestion';
+import QuizEnd from './QuizEnd';
 
 let quizData = require('./quiz_data.json');
 
 class Quiz extends Component {
-
     render() {
+        const isQuizEnd = this.state.quiz_position - 1 === quizData.quiz_questions.length;
         return (
-            <QuizQuestion quiz_question={quizData.quiz_questions[0]} />
+            <div>                
+                {isQuizEnd ? <QuizEnd /> : "" }
+                <QuizQuestion quiz_question={quizData.quiz_questions[0]} />
+            </div>
         );
     };
     constructor(props) {
         super(props);
         this.state = {quiz_position : 1};
+        
     };
 
 }
